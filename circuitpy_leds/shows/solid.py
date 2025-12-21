f1f6e35ee06a87cefde5d87f27da65a47145db1c
Andreas Würl <andi@tryb.de>
2025-12-22 00:17:09 +0100

    validate color

--- circuitpy_leds/shows/solid.py
+++ circuitpy_leds/shows/solid.py
@@ -2,18 +2,17 @@ import asyncio
 
 from .. import Strip
 from ..support.blend import SmoothBlend
+from ..support.color import validate_color
 
 
 class Solid:
 
-    def __init__(self, strip: Strip, color: tuple):
+    def __init__(self, strip: Strip, color: list[int] | tuple = (255, 255, 255)):
         self.strip = strip
-        self.color = color
+        self.color = validate_color(color)
         self.blend = None
 
-
     async def execute(self, _):
-        print("Solid execute")
         if self.blend is None:
             self.blend = SmoothBlend(self.strip, self.color)
 
